8006480c8cad0f56eb0f6ecf2c456e187f71ef24
Dibyadhir <dibyakantid@brightcomgroup.com>
2022-12-24 17:49:34 +0000

    twodigit printing

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(10):
         if i == 1 and j == 2:
             print("12")
         elif i < j:
-            print('{:d}{:d}'.format(i,j),end = ", ")`
+            print('{:d}{:d}' .format(i, j), end = ", ")`
